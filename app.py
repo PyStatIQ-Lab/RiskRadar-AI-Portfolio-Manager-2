6118e4e27ab36f85e9078612406cc740e5c4f876
PyStatIQ-Lab <support@pystatiq.com>
2025-04-14 11:02:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.markdown("""
     }
     
     .st-bw {
-        background-color: white;
+        background-color: blue;
         border-radius: 10px;
         padding: 20px;
         box-shadow: 0 2px 10px rgba(0,0,0,0.1);
